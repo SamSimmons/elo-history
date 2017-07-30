5385730d9ca083c62436c5d525282e6679f96854
sam simmons <samuel.j.simmons@gmail.com>
2017-07-30 23:19:32 +1200

    color lines, still not responsive and no axis yet

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,22 +2,50 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { scaleLinear, line } from 'd3'
 
+const getLine = (team, data) => {
+  const width = 300
+  const height = 180
+  const xScale = scaleLinear().domain([data[0].date, data[data.length - 1].date]).range([0, width])
+  const yScale = scaleLinear().domain([1200, 2000]).range([height, 0])
+  return line()
+  .x(d => xScale(d.date))
+  .y(d => {
+    return yScale(d.ratings[team])
+  })(data)
+}
 
 const Chart = (props) => {
   if (!props.data.length) {
     return <div className="chart"></div>
   }
-
-  const width = 300
-  const height = 180
-
-  const xScale = scaleLinear().domain([props.data[0].date, props.data[props.data.length - 1].date]).range([0, width])
-  const yScale = scaleLinear().domain([1200, 2000]).range([height, 0])
-  const getLine = line().x(d => xScale(d.ratings.Australia)).y(d => yScale(d.date))
+  const colors = [
+    'tomato',
+    'steelblue',
+    'CadetBlue',
+    'Crimson',
+    'DarkOliveGreen',
+    'IndianRed',
+    'LightBlue',
+    'LightSalmon',
+    'Orange'
+  ]
   return (
     <div className="chart">
       <svg style={{width: 300, height: 180}}>
-        <path d={getLine(props.data)} />
+        {
+          props.teams.map(
+            (team, i) =>
+              <path
+                key={`team-line-${i}`}
+                d={getLine(team, props.data)}
+                style={{
+                  stroke: colors[i],
+                  strokeWidth: '1px',
+                  fill: 'transparent'
+                }}
+              />
+          )
+        }
       </svg>
     </div>
   )
